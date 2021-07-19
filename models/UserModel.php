3cb5d064b55a420b84a5f8af1ad4923df4b490df
spawn2010 <87033298+spawn2010@users.noreply.github.com>
2021-07-19 10:17:40 +0300

    Update UserModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -2,21 +2,6 @@
 
 class UserModel extends Model
 {
-    /*
-    public function checkUser()
-    {
-        $login = ($_POST['login']);
-        $password = (($_POST['pass']));
-        $sql = "SELECT * FROM data WHERE login = :login AND pass = :password";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindValue(":login", $login, PDO::PARAM_STR);
-        $stmt->bindValue(":password", $password, PDO::PARAM_STR);
-        $stmt->execute();
-
-        $this->result = $stmt->fetch(PDO::FETCH_ASSOC);;
-    }
-    */
-
     public function renderProblem()
     {
         $sql = "SELECT * FROM problem ";
